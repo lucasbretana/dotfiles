bc15904d71d65734c6b95e3c40b5920f436531be
Lucas Eduardo Bretana <lebretana@inf.ufpel.edu.br>
2019-10-21 23:02:25 -0300

    i3blocks default scripts, and install for ./bin/ files

--- install.sh
+++ install.sh
@@ -52,6 +52,19 @@ install ()
   esac
 }
 
+# link files under ./bin to corresponding
+# destination, as like
+# ./rick_lock.sh -> ~/.bin
+# NOTE: will create ~/bin if necessary
+install_bin ()
+{
+  for i in $(ls ./bin);
+  do
+    mkdir -p ~/.bin;
+    ln -si $(pwd)/bin/"$i" ~/.bin/"$i";
+  done
+}
+
 # check for files to be installed, or list them
 # params: list of files to be installed or nothing
 check ()
@@ -61,7 +74,7 @@ check ()
     echo "You did not specify the files";
     echo "So, should I install all those in this folder and res/*? (y/n)";
     echo "ps: note that I will not do this recursively";
-    echo "ps: note that I will not list files that end in `private`";
+    echo "ps: note that I will not list files that end in 'private'";
     read -n 1 ANS;
     echo " ";
 
@@ -77,14 +90,12 @@ check ()
 	;;
       [nN]*)
         echo -e "\nWell, aborting then";
-        exit $OK;;
+	return $ERR_ABRT;;
       *)
         echoerr "Invalid option, killing it";
         exit $ERR_INV_OPT;;
     esac
   fi
-
-  _list $FILES
 }
 
 main ()
@@ -97,12 +108,30 @@ main ()
       exit $ERR_UNKOWN;;
     *)
       check "$@";
-      if [ $? -eq 0 ]; then
+      _ret=$?;
+      if [ ${_ret} -eq 0 ]; then
         install "$FILES";
-      else
-        return $?;
+      elif [ ${_ret} -ne $ERR_ABRT ]; then
+        exit ${_ret};
       fi
   esac
+  while (true)
+  do
+    echo "Install files under \"./bin/\"? (Y/n)";
+    read -n 1 ans;
+    case "${ans}" in
+      [yY]*)
+        install_bin;
+        break;
+        ;;
+      [nN]*)
+        break;
+        ;;
+      *)
+        continue;
+        ;;
+    esac
+  done
 }
 
 help ()
@@ -117,6 +146,7 @@ help ()
   echo -e "ERR_ENV $ERR_ENV";
   echo -e "ERR_ENV_MISS $ERR_ENV_MISS";
   echo -e "ERR_INV_OPT $ERR_INV_OPT";
+  echo -e "ERR_ABRT $ERR_ABRT";
 }
 
 echoerr ()
@@ -132,6 +162,7 @@ ERR_UNKOWN=1;
 ERR_ENV=2;
 ERR_ENV_MISS=3;
 ERR_INV_OPT=4;
+ERR_ABRT=5;
 
 main $@;
 exit $?;
